3a9d768330fbf68fed1ced85bccbb5118414fcae
Abel <li.carlos@upeu.edu.pe>
2019-11-07 19:08:44 -0500

    six commit

--- ecom_serve/resources/views/auth/login.blade.php
+++ ecom_serve/resources/views/auth/login.blade.php
@@ -51,6 +51,8 @@
                             </div>
                         </div>
 
+                        
+
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -63,13 +65,14 @@
                                     </a>
                                 @endif
                             </div>
-                        </div>
-                        <hr>
-                        <div class="form-group row mb-0">
+                            <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <a href="{{ url('/auth/redirect/facebook') }}" class="btn btn-primary"><i class="fa fa-facebook"></i> Facebook</a>
                             </div>
                         </div>
+                        </div>
+                        <hr>
+                        
                     </form>
                 </div>
             </div>
