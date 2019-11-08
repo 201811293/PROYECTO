031066ab3a749aac6c07c1993a0c71e25b009f64
Abel <li.carlos@upeu.edu.pe>
2019-11-07 21:54:35 -0500

    apiRest

--- ecom_serve/routes/api.php
+++ ecom_serve/routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 
+Route::group([
+    'prefix' => 'auth',
+], function () {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
